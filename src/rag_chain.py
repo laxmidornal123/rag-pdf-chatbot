d08d2f42a50f804f091bb6be4bc3976c827fe2da
Laxmi Dornal <laxmidornal12@gmail.com>
2026-06-19 22:53:27 +0530

    Fix requirements

--- src/rag_chain.py
+++ src/rag_chain.py
@@ -1,16 +1,12 @@
 from dotenv import load_dotenv
 import streamlit as st
-
-import os
-
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
-
-google_api_key=st.secrets["GOOGLE_API_KEY"]
+    google_api_key=st.secrets["GOOGLE_API_KEY"],
     temperature=0.3
 )
 
